e8cbb11944799974ee5b03ce1c6a0abf9c476fb5
victoriathomp <vthomp2222@gmail.com>
2025-09-24 12:55:16 -0400

    commit for ARGOSTrackingTool to process all lines using a while loop

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -15,23 +15,27 @@ file_name = './data/raw/sara.txt'
 file_object = open(file_name,'r')
 
 #Read contents of file into a list
-line_list = file_object.readlines()
-
-#Close the file
-file_object.close()
+lineString = file_object.readline()
 
 #Pretend we read one line of data from the file
-lineString = line_list[200]
+while lineString:
+    # Check if line is a data line
+    if lineString[0] in ("#","u"):
+        lineString = file_object.readline()
+        continue
 
-#Split the string into a list of data items
-lineData = lineString.split()
+    #Split the string into a list of data items
+    lineData = lineString.split()
 
-#Extract items in list into variables
-record_id = lineData[0]
-obs_date = lineData[2]
-obs_lc = lineData[4]
-obs_lat = lineData[6]
-obs_lon = lineData[7]
+    #Extract items in list into variables
+    record_id = lineData[0]
+    obs_date = lineData[2]
+    obs_lc = lineData[4]
+    obs_lat = lineData[6]
+    obs_lon = lineData[7]
 
-#Print the location of sara
-print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
+    #Print the location of sara
+    print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
+
+    #Read next line
+    lineString = file_object.readline()
